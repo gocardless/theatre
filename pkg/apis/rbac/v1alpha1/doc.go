f9e624db5a91b8d6086f800551df0cf095636310
Lawrence Jones <lawrjone@gmail.com>
2018-12-21 13:03:52 +0000

    Clean-up

--- pkg/apis/rbac/v1alpha1/doc.go
+++ pkg/apis/rbac/v1alpha1/doc.go
@@ -1,5 +1,8 @@
-// +k8s:deepcopy-gen=package,register
-
 // Package v1alpha1 is the v1alpha1 version of the GoCardless RBAC API
+
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen=package,register
+// +k8s:conversion-gen=github.com/lawrencejones/rbac-directory/pkg/apis/rbac
+// +k8s:defaulter-gen=TypeMeta
 // +groupName=rbac.gocardless.io
 package v1alpha1

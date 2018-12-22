89553f714b237fba5f7b6166266e0d7ea6b0533c
Lawrence Jones <lawrjone@gmail.com>
2018-12-22 19:20:22 +0000

    Test for unregistered API groups

---
+++ pkg/apis/apis.go
@@ -0,0 +1,19 @@
+package apis
+
+import (
+	"k8s.io/apimachinery/pkg/runtime"
+
+	rbacv1alpha1 "github.com/lawrencejones/theatre/pkg/apis/rbac/v1alpha1"
+)
+
+var (
+	// AddToSchemes collects all the AddToScheme functions from each of our API groups into
+	// a single SchemeBuilder, which can then provide the single function required to add
+	// those resources to a Scheme.
+	AddToSchemes = runtime.SchemeBuilder{
+		rbacv1alpha1.AddToScheme,
+	}
+
+	// AddToScheme when called with a Scheme will add all our CRDs into the RESTClient
+	AddToScheme = AddToSchemes.AddToScheme
+)

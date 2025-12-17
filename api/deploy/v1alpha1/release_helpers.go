b5e6b8d36cc91cfa9decb2b8f828a5ccb69fe197
Emil Lozev <elozev@gocardless.com>
2025-12-17 18:52:53 +0100

    chore: merge two helper files

--- api/deploy/v1alpha1/release_helpers.go
+++ api/deploy/v1alpha1/release_helpers.go
@@ -1,11 +1,43 @@
 package v1alpha1
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/json"
 	"sort"
 
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+func (rc *ReleaseConfig) Equals(other *ReleaseConfig) bool {
+	return bytes.Equal(rc.Serialise(), other.Serialise())
+}
+// The serialisation is used to determine if a release has changed.
+// For release uniqueness we only take into consideration the target name,
+// revision.name and revision.id.
+func (rc *ReleaseConfig) Serialise() []byte {
+	canonical := ReleaseConfig{
+		TargetName: rc.TargetName,
+		Revisions:  rc.Revisions,
+	}
+
+	for _, revision := range canonical.Revisions {
+		var canonicalRevision Revision
+		canonicalRevision.Name = revision.Name
+		canonicalRevision.ID = revision.ID
+
+		canonical.Revisions = append(canonical.Revisions, canonicalRevision)
+	}
+
+	sort.Slice(canonical.Revisions, func(i, j int) bool {
+		return canonical.Revisions[i].Name < canonical.Revisions[j].Name
+	})
+
+	bytes, _ := json.Marshal(canonical)
+
+	return bytes
+}
 
 func (r *Release) IsStatusInitialised() bool {
 	return r.Status.ObservedGeneration > 0

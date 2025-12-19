6693e0af7a4ad0fb372a2d19cc51d03495fa2794
Emil Lozev <elozev@gocardless.com>
2025-12-19 11:53:57 +0100

    Release controller boilerplate (#430)

---
+++ internal/controller/deploy/messages.go
@@ -0,0 +1,8 @@
+package deploy
+
+const (
+	MessageReleaseCreated    = "Release has been just created, waiting for status update"
+	MessageReleaseActive     = "Release is active"
+	MessageReleaseInactive   = "Release is inactive"
+	MessageReleaseSuperseded = "Release is superseded by %s"
+)

030443ef4eb19b60a2d7db02e8e3dce869529ac5
Gusts Jonasts <101050452+0x0013@users.noreply.github.com>
2026-01-12 18:11:30 +0200

    Update ginkgo to v2 (#439)

--- internal/controller/deploy/release_controller_test.go
+++ internal/controller/deploy/release_controller_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/gocardless/theatre/v5/api/deploy/v1alpha1"
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"

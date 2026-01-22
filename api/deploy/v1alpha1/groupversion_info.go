fe1d5cf5646ca0e23b23788c6d289ae6f2ffc54f
Emil Lozev <elozev@gocardless.com>
2026-01-22 14:36:25 +0000

    Release controller: fix name uniqueness flag default value + CRD full name (#448)

--- api/deploy/v1alpha1/groupversion_info.go
+++ api/deploy/v1alpha1/groupversion_info.go
@@ -1,6 +1,6 @@
 // Package v1alpha1 contains API Schema definitions for the deploy v1alpha1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=deploy.gocardless.com
+// +groupName=deploy.crd.gocardless.com
 package v1alpha1
 
 import (
@@ -10,7 +10,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "deploy.gocardless.com", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "deploy.crd.gocardless.com", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}

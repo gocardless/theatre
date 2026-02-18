624af42b9d7d9f561a2f6c3f2b08dd9f7b048e6c
Emil Lozev <elozev@gocardless.com>
2026-02-18 20:13:10 +0100

    feat: adding first rough version of the automated rollback controller

---
+++ cmd/automated-rollback-manager/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/alecthomas/kingpin"
+	deployv1alpha1 "github.com/gocardless/theatre/v5/api/deploy/v1alpha1"
+	"github.com/gocardless/theatre/v5/cmd"
+
+	"github.com/gocardless/theatre/v5/pkg/signals"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	automatedrollbackcontroller "github.com/gocardless/theatre/v5/internal/controller/deploy"
+)
+
+var (
+	scheme        = runtime.NewScheme()
+	app           = kingpin.New("automated-rollback-manager", "Creates rollback resources based on release status and rollback policies").Version(cmd.VersionStanza())
+	commonOptions = cmd.NewCommonOptions(app).WithMetrics(app)
+)
+
+func init() {
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(deployv1alpha1.AddToScheme(scheme))
+}
+
+func main() {
+	kingpin.MustParse(app.Parse(os.Args[1:]))
+	logger := commonOptions.Logger()
+
+	ctx, cancel := signals.SetupSignalHandler()
+	defer cancel()
+
+	webhookOpts := webhook.Options{Port: 9443}
+	webhookServer := webhook.NewServer(webhookOpts)
+
+	manager, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		LeaderElection:   commonOptions.ManagerLeaderElection,
+		LeaderElectionID: "rollbackpolicy.deploy.crd.gocardless.com",
+		Scheme:           scheme,
+		Metrics: metricsserver.Options{
+			BindAddress: fmt.Sprintf("%s:%d", commonOptions.MetricAddress, commonOptions.MetricPort),
+		},
+		WebhookServer: webhookServer,
+	})
+
+	if err != nil {
+		app.Fatalf("failed to create manager: %v", err)
+	}
+
+	if err = (&automatedrollbackcontroller.AutomatedRollbackReconciler{
+		Client: manager.GetClient(),
+		Scheme: scheme,
+		Log:    logger,
+	}).SetupWithManager(ctx, manager); err != nil {
+		app.Fatalf("failed to create controller: %v", err)
+	}
+
+	if err := manager.Start(ctx); err != nil {
+		app.Fatalf("failed to start manager: %v", err)
+	}
+}
---
+++ internal/controller/deploy/automated_rollback_controller.go
@@ -0,0 +1,206 @@
+package deploy
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-logr/logr"
+	deployv1alpha1 "github.com/gocardless/theatre/v5/api/deploy/v1alpha1"
+	cicd "github.com/gocardless/theatre/v5/pkg/cicd/github"
+	"github.com/gocardless/theatre/v5/pkg/recutil"
+	apiextv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+)
+
+const RollbackPolicyIndexFieldTargetName = ".spec.targetName"
+
+type AutomatedRollbackReconciler struct {
+	client.Client
+	Log    logr.Logger
+	Scheme *runtime.Scheme
+}
+
+func (r *AutomatedRollbackReconciler) SetupWithManager(ctx context.Context, mgr ctrl.Manager) error {
+	logger := r.Log.WithValues("component", "AutomatedRollback")
+
+	ctrlBuilder := ctrl.NewControllerManagedBy(mgr).
+		Named("automated_rollback").
+		For(&deployv1alpha1.Release{}).
+		// Owns(&deployv1alpha1.Rollback{}).
+		WithEventFilter(r.onReleaseConditionsChangedPredicate())
+
+	err := mgr.GetFieldIndexer().IndexField(
+		ctx,
+		&deployv1alpha1.RollbackPolicy{},
+		RollbackPolicyIndexFieldTargetName,
+		func(rawObj client.Object) []string {
+			rollbackPolicy := rawObj.(*deployv1alpha1.RollbackPolicy)
+			return []string{rollbackPolicy.Spec.TargetName}
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	err = mgr.GetFieldIndexer().IndexField(
+		ctx,
+		&deployv1alpha1.Rollback{},
+		IndexFieldOwner,
+		func(rawObj client.Object) []string {
+			run := rawObj.(*deployv1alpha1.Rollback)
+			owner := metav1.GetControllerOf(run)
+			if owner == nil {
+				return nil
+			}
+			if owner.APIVersion != apiGVStr || owner.Kind != "Release" {
+				return nil
+			}
+			return []string{owner.Name}
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	return ctrlBuilder.Complete(
+		recutil.ResolveAndReconcile(
+			ctx, logger, mgr, &deployv1alpha1.Release{},
+			func(logger logr.Logger, request ctrl.Request, obj runtime.Object) (ctrl.Result, error) {
+				return r.Reconcile(ctx, logger, request, obj.(*deployv1alpha1.Release))
+			},
+		),
+	)
+}
+
+func (r *AutomatedRollbackReconciler) Reconcile(ctx context.Context, logger logr.Logger, request ctrl.Request, release *deployv1alpha1.Release) (ctrl.Result, error) {
+	logger.Info("Automated rollback reconciler for release ", "release", release.Name)
+	if !release.IsConditionActiveTrue() {
+		// nothing to do, exit early
+		return ctrl.Result{}, nil
+	}
+
+	if hasRollback, err := r.hasRollback(ctx, release); err != nil {
+		return ctrl.Result{}, err
+	} else if hasRollback {
+		// nothing to do, exit early
+		return ctrl.Result{}, nil
+	}
+
+	// fetch the rollback policy for that target name
+	var err error
+	var rollbackPolicy *deployv1alpha1.RollbackPolicy
+	if rollbackPolicy, err = r.getRollbackPolicy(ctx, release); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if !rollbackAllowed(rollbackPolicy) {
+		// nothing to do, exit early
+		return ctrl.Result{}, nil
+	}
+
+	if err := r.createRollback(ctx, release); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	return ctrl.Result{}, nil
+}
+
+func (r *AutomatedRollbackReconciler) onReleaseConditionsChangedPredicate() predicate.Predicate {
+	return predicate.Funcs{
+		// TODO: filter out only releases that are active and require rollback
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			return e.ObjectNew.(*deployv1alpha1.Release).IsConditionActiveTrue()
+		},
+		CreateFunc: func(e event.CreateEvent) bool {
+			return e.Object.(*deployv1alpha1.Release).IsConditionActiveTrue()
+		},
+		DeleteFunc: func(e event.DeleteEvent) bool {
+			return false
+		},
+	}
+}
+
+func rollbackAllowed(rollbackPolicy *deployv1alpha1.RollbackPolicy) bool {
+	// TODO: expand this to handle the rest of the fields in the spec.automated
+	return rollbackPolicy.Spec.Automated.Enabled
+}
+
+func (r *AutomatedRollbackReconciler) createRollback(ctx context.Context, release *deployv1alpha1.Release) error {
+	// these deployment options are git specific, maybe we need a template for this?
+	deploymentOptions := map[string]apiextv1.JSON{
+		// Forces a deployment to be created immediately, skipping the dispatcher queue
+		// https://github.com/gocardless/dispatcher/blob/master/pkg/deployments/deployments.go#L43
+		"skip_queue":                   mustMarshal(true),
+		cicd.DeploymentRevisionNameKey: mustMarshal("application"),
+		"target_revision":              mustMarshal("{.config.revisions[?(@.name==\"infrastructure\")].id}"),
+		"version":                      mustMarshal(3),
+		"environment":                  mustMarshal(release.Labels["environment"]),
+	}
+
+	spec := deployv1alpha1.RollbackSpec{
+		Reason: "automated rollback",
+		InitiatedBy: deployv1alpha1.RollbackInitiator{
+			Principal: "system",
+			Type:      "system",
+		},
+		DeploymentOptions: deploymentOptions,
+	}
+
+	rb := &deployv1alpha1.Rollback{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: fmt.Sprintf("%s-", release.TargetName),
+			Namespace:    release.Namespace,
+			Labels:       release.Labels,
+		},
+		Spec: spec,
+	}
+
+	if err := r.Create(ctx, rb); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *AutomatedRollbackReconciler) hasRollback(ctx context.Context, release *deployv1alpha1.Release) (bool, error) {
+	rollbackList := &deployv1alpha1.RollbackList{}
+	if err := r.List(ctx, rollbackList, client.InNamespace(release.Namespace), client.MatchingFields(map[string]string{IndexFieldOwner: release.Name})); err != nil {
+		return false, fmt.Errorf("failed to list rollbacks: %w", err)
+	}
+
+	return len(rollbackList.Items) > 0, nil
+}
+
+func (r *AutomatedRollbackReconciler) getRollbackPolicy(ctx context.Context, release *deployv1alpha1.Release) (*deployv1alpha1.RollbackPolicy, error) {
+	target := release.ReleaseConfig.TargetName
+
+	rollbackPolicyList := &deployv1alpha1.RollbackPolicyList{}
+	matchFields := client.MatchingFields(map[string]string{RollbackPolicyIndexFieldTargetName: target})
+	if err := r.List(ctx, rollbackPolicyList, client.InNamespace(release.Namespace), matchFields); err != nil {
+		return nil, fmt.Errorf("failed to list rollback policies: %w", err)
+	}
+
+	if len(rollbackPolicyList.Items) == 0 {
+		return nil, fmt.Errorf("no rollback policies found for target: %s", target)
+	}
+
+	if len(rollbackPolicyList.Items) > 1 {
+		return nil, fmt.Errorf("multiple rollback policies found for target: %s", target)
+	}
+
+	return &rollbackPolicyList.Items[0], nil
+}
+
+func mustMarshal(s any) apiextv1.JSON {
+	b, err := json.Marshal(s)
+	if err != nil {
+		panic(err)
+	}
+	return apiextv1.JSON{Raw: b}
+}

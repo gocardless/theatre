931ae3c6cc9970db4b832c3f8773f6befc73401b
Gusts Jonasts <101050452+0x0013@users.noreply.github.com>
2026-03-11 12:59:05 +0200

    test(runner): add unit tests

---
+++ pkg/workloads/console/runner/suite_test.go
@@ -0,0 +1,13 @@
+package runner
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestSuite(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "pkg/workloads/console/runner")
+}

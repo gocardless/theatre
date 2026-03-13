c798ea989d7987b8381848027516d748aa67df65
Emil Lozev <elozev@gocardless.com>
2026-03-13 14:59:34 +0000

    AutomatedRollbackPolicy: validating webhook (#484)

--- cmd/automated-rollback-manager/main.go
+++ cmd/automated-rollback-manager/main.go
@@ -14,8 +14,11 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	automatedrollbackcontroller "github.com/gocardless/theatre/v5/internal/controller/deploy"
+	automatedrollbackwebhook "github.com/gocardless/theatre/v5/internal/webhook/deploy/v1alpha1/automated-rollback-policy"
 )
 
 var (
@@ -36,6 +39,9 @@ func main() {
 	ctx, cancel := signals.SetupSignalHandler()
 	defer cancel()
 
+	webhookOpts := webhook.Options{Port: 9443}
+	webhookServer := webhook.NewServer(webhookOpts)
+
 	manager, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		LeaderElection:   commonOptions.ManagerLeaderElection,
 		LeaderElectionID: "automated-rollback-policy.deploy.crd.gocardless.com",
@@ -43,6 +49,7 @@ func main() {
 		Metrics: metricsserver.Options{
 			BindAddress: fmt.Sprintf("%s:%d", commonOptions.MetricAddress, commonOptions.MetricPort),
 		},
+		WebhookServer: webhookServer,
 	})
 
 	if err != nil {
@@ -56,6 +63,10 @@ func main() {
 	}).SetupWithManager(ctx, manager); err != nil {
 		app.Fatalf("failed to create controller: %v", err)
 	}
+
+	manager.GetWebhookServer().Register("/validate-automated-rollback-policies", &admission.Webhook{
+		Handler: automatedrollbackwebhook.NewAutomatedRollbackPolicyValidateWebhook(logger, manager.GetScheme(), manager.GetClient()),
+	})
 
 	if err := manager.Start(ctx); err != nil {
 		app.Fatalf("failed to start manager: %v", err)
